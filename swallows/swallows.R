ec8e032d9708522040ded261a1793b170dfefe96
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2025-04-17 10:15:47 -0700

    Update swallows and add plot

--- swallows/swallows.R
+++ swallows/swallows.R
@@ -1,55 +1,58 @@
 library(RTMB)
 
 data <- readRDS('data.RDS')
-inits <- function()
-  list(sigmayearphi=runif(1, 0, 2),
-       sigmaphi=runif(1,0,2),
-       sigmap=runif(1,0,2),
-       a=rnorm(data$K-1, 3, 1), a1=rnorm(1, .5, 1),
-       b0=rnorm(4, 3, sd=1), b1=rnorm(4, 0, .15),
-       fameffphi_raw=rnorm(data$nfam,0,1),
-       fameffp_raw=rnorm(data$nfam,0,1),
-       yeareffphi_raw=rnorm(4, 0,1))
-set.seed(23254)
-inits <- inits()
-random <- c("fameffphi_raw", "fameffp_raw", "yeareffphi_raw")
+# Pick some arbitrary birds to examine survival probabilities.
+# Makes sense to pick one from each family from a different year
+# and survives more than a few periods
+library(dplyr)
+set.seed(1021)
+meta <- with(data, data.frame(id=1:I, family, last, year))
+data$id <- group_by(meta, family) |> filter(last > 5) |> slice_sample(n=1) |>
+  group_by(year) |> slice_sample(n=1) |> pull(id)
 
+# initial parameter list
+pars <- list(logsigmayearphi=1,
+       logsigmaphi=-1,
+       logsigmap=0,
+       a=rep(1,data$K-1), a1=.5,
+       b0=rep(3,4), b1=rep(0,4),
+       fameffphi_raw=rep(.1,data$nfam),
+       fameffp_raw=rep(.1, data$nfam),
+       yeareffphi_raw=rep(.1,4))
 
+# The RTMB model
 func <- function(pars){
   getAll(pars,data)
-  nlp <- 0
   p <- matrix(1, I, K)
   phi <- matrix(0, I, K-1)
   chi <- matrix(1, I, K+1)
-  sigmayearphi2 <- exp(sigmayearphi)
-  sigmaphi2 <- exp(sigmaphi)
-  sigmap2 <- exp(sigmap)
+  sigmayearphi <- exp(logsigmayearphi)
+  sigmaphi <- exp(logsigmaphi)
+  sigmap <- exp(logsigmap)
   
   # priors
   nlp <- -sum(dnorm(b0, 0, 5, TRUE))-
     sum(dnorm(b1, 0, 5, TRUE))-
     sum(dnorm(a, 0, 1.5, TRUE))-
     dnorm(a1, 0, 5, TRUE)-
-    dcauchy(sigmaphi2, 0, 1, TRUE)-
-    dnorm(sigmayearphi2, 0, 2, TRUE)-
-    dcauchy(sigmap2, 0, 1, TRUE)
+    dcauchy(sigmaphi, 0, 1, TRUE)-
+    dnorm(sigmayearphi, 0, 2, TRUE)-
+    dcauchy(sigmap, 0, 1, TRUE)
   # Jacobians and hyper distributions
-  nll <- -sigmaphi-sigmayearphi-sigmap-
+  nll <- -logsigmaphi-logsigmayearphi-logsigmap-
     sum(dnorm(fameffphi_raw, 0, 1, TRUE))-
     sum(dnorm(fameffp_raw,0, 1, TRUE))-
     sum(dnorm(yeareffphi_raw, 0, 1, TRUE))
   for(i in 1:I){
     phi[i,1:(K-1)] <- plogis(a[1:(K-1)] + a1*carez[i] +
-                              sigmayearphi2*yeareffphi_raw[year[i]] +
-                              sigmaphi2*fameffphi_raw[family[i]])
+                              sigmayearphi*yeareffphi_raw[year[i]] +
+                              sigmaphi*fameffphi_raw[family[i]])
     p[i,2:K] <- plogis(b0[year[i]]+ b1[year[i]]*agec[2:K]+
-                         sigmap2*fameffp_raw[family[i]])
-    k <- K
-    while (k > 1) {
-      chi[i,k] <- (1 - phi[i,k-1]) + phi[i,k-1] * (1 - p[i,k])*chi[i,k+1]
-      k <- k - 1
+                         sigmap*fameffp_raw[family[i]])
+    for(k in K:2){
+      chi[i,k] <- (1-phi[i,k-1]) + phi[i,k-1] * (1-p[i,k])*chi[i,k+1]
     }
-    chi[i,1] <- (1 - p[i,1]) * chi[i,2]
+    chi[i,1] <- (1-p[i,1]) * chi[i,2]
     # likelihood of the data  
     nll <- nll -
       # probability of survival, known alive since k<last
@@ -60,14 +63,61 @@ func <- function(pars){
       log(chi[i,last[i]+1])
   }
   nld <- nll+nlp # negative log density
+  prep <- p[id,] # report a subset of individuals
+  REPORT(prep)
+  logit_prep <- log(prep)-log(1-prep)
+  ADREPORT(logit_prep)
   REPORT(nld)
-  REPORT(chi)
-  REPORT(p)
-  REPORT(phi)
   return(nld)
 }
+func(pars)
 
-obj <- MakeADFun(func, inits, random=random, silent=TRUE)
-obj$report()$nld - func(inits)
+obj <- MakeADFun(func, pars, silent=TRUE,
+                 random=c("fameffphi_raw", "fameffp_raw", "yeareffphi_raw"))
 opt <- with(obj, nlminb(par,fn,gr))
-sdreport(obj)
+sum(opt$par) #  14.41553
+# confidence intervals of survival probailities of selected birds
+prep.ml <- summary(sdreport(obj), 'report') |> as.data.frame() |>
+  setNames(c('Estimate', 'SE')) |> 
+  # CI in logit space
+  mutate(lwr=Estimate-1.96*SE, upr=Estimate+1.96*SE) |>
+  # CI in natural space
+  mutate(Estimate=plogis(Estimate), lwr=plogis(lwr), upr=plogis(upr)) |>
+  # add ID data for plotting
+  mutate(id=rep(data$id, times=data$K), k=rep(1:data$K, each=length(data$id)))
+
+
+# Run MCMC on the model
+library(adnuts)
+mcmc <- sample_sparse_tmb(obj, iter=1500, warmup=500,  seed=1231,
+                          control=list(adapt_delta=.99),
+                          globals=list(data=data, pars=pars))
+
+# loop through each posterior sample and call report to get
+# posterior distributions
+post <- extract_samples(mcmc)
+prep.all <- list()
+for(i in 1:nrow(post)){
+  tmp <- obj$report(as.numeric(post[i,]))
+  prep.all[[i]] <- reshape2::melt(tmp$prep) |> 
+    setNames(c('id', 'k', 'surv_prob')) |>
+    cbind(iter=i)
+}
+prep <- do.call(rbind, prep.all) 
+prep$id <- data$id[prep$id]
+
+prep.bayes <- group_by(prep, id, k) |>
+  summarize(Estimate=median(surv_prob),
+         lwr=quantile(surv_prob, .025),
+         upr=quantile(surv_prob, 0.975), .groups='drop')
+
+library(ggplot2)
+theme_set(theme_bw())
+# Bayesian credible intervals vs asymptotic confidence intervals
+g <- ggplot(prep.bayes, aes(k, y=Estimate, ymin=lwr, ymax=upr)) + 
+  geom_line(color=gray(.5)) +
+  geom_ribbon(alpha=.5) + facet_wrap('id') +
+  geom_pointrange(data=prep.ml, fatten = 1, color=1) + 
+  labs(x='Capture period', y='Survival probability')
+ggsave('swallows.png', g, width=6, height=4, units='in')
+
